f8316f025582f0a96ef646e554e243f53c9316e3
juhokarppinen <juho.karppinen@gmail.com>
2017-09-06 22:24:40 +0300

    Add Brick Breaker to version control

---
+++ 1-Brick-Breaker/Assets/BallController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour
+{
+	public float fallDepth;
+	public float startSpeed;
+
+	private float defaultX;
+	private float defaultY;
+	private float defaultZ;
+
+	private Rigidbody rigidBody;
+	private PaddleController paddle;
+
+	// Use this for initialization
+	void Start ()
+	{
+		defaultX = transform.position.x;
+		defaultY = transform.position.y;
+		defaultZ = transform.position.z;
+		rigidBody = GetComponent<Rigidbody> ();
+		paddle = FindObjectOfType<PaddleController> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButtonDown ("Fire1")) {
+			Shoot ();
+		}
+		if (HasFallen ()) {
+			Replace ();
+		}
+		if (rigidBody.velocity.magnitude < 0.001f) {
+			transform.position = new Vector3 (paddle.X, defaultY, defaultZ);
+		}
+	}
+
+
+	void FixedUpdate ()
+	{
+	}
+
+	private void Replace ()
+	{
+		rigidBody.velocity = Vector3.zero;
+		transform.position = new Vector3 (defaultX, defaultY, defaultZ);
+	}
+
+	private void Shoot ()
+	{
+		if (rigidBody.velocity.magnitude > 0)
+			return;
+		Vector3 force = new Vector3 (Random.value - .5f, 0, Random.value);
+		force.Normalize ();
+		rigidBody.AddForce (force * startSpeed);
+	}
+
+	private bool HasFallen ()
+	{
+		return transform.position.y <= fallDepth;
+	}
+}
---
+++ 1-Brick-Breaker/Assets/PaddleController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleController : MonoBehaviour
+{
+	public float speedScale;
+
+	private const float playAreaWidth = 10f;
+	private new Renderer renderer;
+
+	public float X {
+		get { return transform.position.x; }
+	}
+
+	private float Y {
+		get { return transform.position.y; }
+	}
+
+	private float Z {
+		get { return transform.position.z; }
+	}
+
+	private float HalfSize {
+		get { return renderer.bounds.extents.x; }
+	}
+
+	private float Left {
+		get { return X - HalfSize; }
+		set { transform.position = new Vector3 (value + HalfSize, Y, Z); }
+	}
+
+	private float Right {
+		get { return X + HalfSize; }
+		set { transform.position = new Vector3 (value - HalfSize, Y, Z); }
+	}
+
+	void Start ()
+	{
+		renderer = GetComponentInChildren<Renderer> ();
+	}
+
+	void FixedUpdate ()
+	{
+		float horizontalMovement = Input.GetAxisRaw ("Horizontal");
+		float x = X + horizontalMovement * Time.deltaTime * speedScale;
+		transform.position = new Vector3 (x, Y, Z);
+		if (Left < -playAreaWidth)
+			Left = -playAreaWidth;
+		if (Right > playAreaWidth)
+			Right = playAreaWidth;
+	}
+}
