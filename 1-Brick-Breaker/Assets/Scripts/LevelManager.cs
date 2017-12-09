bf0680d84d06b478541d400f0de457e908704319
juhokarppinen <juho.karppinen@gmail.com>
2017-12-10 01:17:03 +0200

    Probably build related changes

--- 1-Brick-Breaker/Assets/Scripts/LevelManager.cs
+++ 1-Brick-Breaker/Assets/Scripts/LevelManager.cs
@@ -16,6 +16,14 @@ public class LevelManager : MonoBehaviour
 		FIRST_LEVEL,
 		L_00,
 		L_01,
+		L_02,
+		L_03,
+		L_04,
+		L_05,
+		L_06,
+		L_07,
+		L_08,
+		L_09
 	}
 
 	public Dictionary<Level,string> levels;
@@ -32,7 +40,15 @@ public class LevelManager : MonoBehaviour
 			{ Level.GAMEWIN, "Menu_Win" },
 			{ Level.FIRST_LEVEL, "Game_Level_00" },
 			{ Level.L_00, "Game_Level_00" },
-			{ Level.L_01, "Game_Level_00" }
+			{ Level.L_01, "Game_Level_01" },
+			{ Level.L_02, "Game_Level_02" },
+			{ Level.L_03, "Game_Level_03" },
+			{ Level.L_04, "Game_Level_04" },
+			{ Level.L_05, "Game_Level_05" },
+			{ Level.L_06, "Game_Level_06" },
+			{ Level.L_07, "Game_Level_07" },
+			{ Level.L_08, "Game_Level_08" },
+			{ Level.L_09, "Game_Level_09" },
 		};
 	}
 
@@ -53,5 +69,33 @@ public class LevelManager : MonoBehaviour
 	public void NextLevel ()
 	{
 		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+
+
+	/// <summary>
+	/// Allow loading any level (of the current ten) for testing purposes.
+	/// </summary>
+	void Update ()
+	{
+		if (Input.GetKeyDown ("0"))
+			Load (Level.L_00);
+		if (Input.GetKeyDown ("1"))
+			Load (Level.L_01);
+		if (Input.GetKeyDown ("2"))
+			Load (Level.L_02);
+		if (Input.GetKeyDown ("3"))
+			Load (Level.L_03);
+		if (Input.GetKeyDown ("4"))
+			Load (Level.L_04);
+		if (Input.GetKeyDown ("5"))
+			Load (Level.L_05);
+		if (Input.GetKeyDown ("6"))
+			Load (Level.L_06);
+		if (Input.GetKeyDown ("7"))
+			Load (Level.L_07);
+		if (Input.GetKeyDown ("8"))
+			Load (Level.L_08);
+		if (Input.GetKeyDown ("9"))
+			Load (Level.L_09);
 	}
 }
